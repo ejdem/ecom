1198f0fe4ecfe33b953d0339d44df10494b0508b
adamkr <adam_krawiec93@o2.pl>
2016-01-07 11:43:42 +0000

    cancan for roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   
   protected
   
+  rescue_from CanCan::AccessDenied do |e|
+    flash[:danger] = "access denied!"
+    redirect_to root_url
+  end
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sing_up)        << :name
     devise_parameter_sanitizer.for(:account_update) << :name
