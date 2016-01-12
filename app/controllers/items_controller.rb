29fd7dec7c76836e2956819329aad79ed48f9fa8
adamkr <adam_krawiec93@o2.pl>
2016-01-12 00:28:18 +0000

    sorting items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,19 @@ class ItemsController < ApplicationController
                    .order(params[:sort])
                    .paginate(page: params[:page], per_page: 5)
     else
-      @items = Item.order(params[:sort])
+      @items = Item.order(sort_column + " " + sort_direction)
                    .paginate(page: params[:page], per_page: 5)
     end
+  end
+  
+  private
+  
+  def sort_column
+    Item.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
   # GET /items/1
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    
+    def sorting(col, title = nil)
+        title ||= col.titleize
+        direction = col == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+        link_to title, sort: col, :direction => direction
+    end
 end
