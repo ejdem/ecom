c0bd38981b4f53e163f4c5a713bcea50c55a8b42
adamkr <adam_krawiec93@o2.pl>
2016-01-13 00:34:40 +0000

    sorting with ajax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
         title ||= col.titleize
         direction = col == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
         #link_to title, params.merge(sort: col, :direction => direction, page:nil)
-        link_to title, params.merge(:sort => col, :direction => direction, :page => nil)
+        link_to title, params.merge(:sort => col, :direction => direction, :page => nil), remote: true
     end
 end
